d928785b8f89973011afeb7da5bedf49e3233212
Nandhini <2602403@sit.singaporetech.edu.sg>
2026-09-07 23:19:19 +0800

    Add conditional logic

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ print("Followers after second increase:", followers)
 followers -= 10
 print("Followers after decrease:", followers)
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print()
@@ -29,3 +29,5 @@ print("====================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+if age > 40 and category == "fun":
+    print("You are old what is fun for you??")
